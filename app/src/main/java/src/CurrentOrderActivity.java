670f55acd44e0750e9d62b6b5d141417d0b5fa7b
PA <prarabdhathapa@gmail.com>
2024-12-04 11:07:22 -0500

    Got Pizza Orders To Show

--- app/src/main/java/src/CurrentOrderActivity.java
+++ app/src/main/java/src/CurrentOrderActivity.java
@@ -6,15 +6,37 @@ import android.view.View;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.cs213_project_5.R;
 
+import java.util.ArrayList;
+
+import src.pizzeria.Order;
+import src.pizzeria.Pizza;
+
 public class CurrentOrderActivity extends AppCompatActivity {
+
+    private RecyclerView orderRecyclerView;
+    private PizzaAdapter pizzaAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_current_order); // your XML layout file
+        setContentView(R.layout.activity_current_order); // your XML layout ficurrent
+
+        orderRecyclerView = findViewById(R.id.order_list_view);
+        orderRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        Order currentOrder = Pizzeria.getCurrentOrder();
+        ArrayList<Pizza> pizzas = currentOrder.getPizzas();
+
+        pizzaAdapter = new PizzaAdapter(pizzas, pizza ->
+                Toast.makeText(this, "Selected: " + pizza.getName(), Toast.LENGTH_SHORT).show()
+                );
+
+        orderRecyclerView.setAdapter(pizzaAdapter);
 
         findViewById(R.id.closeButton).setOnClickListener(v -> navigateToMain());
         findViewById(R.id.backButton).setOnClickListener(v -> navigateToCreatePizza());
---
+++ app/src/main/java/src/OrderAdapter.java
@@ -0,0 +1,9 @@
+//package src;
+//
+//import androidx.recyclerview.widget.RecyclerView;
+//
+//public class OrderAdapter extends RecyclerView.Adapter<OrderAdapter.OrderViewHolder> {
+//
+////    private ArrayList<>
+//
+//}
